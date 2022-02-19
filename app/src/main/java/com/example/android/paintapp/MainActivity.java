9e512308193ea0440b0b061b9cb88dbea5cdfc91
TusharSwarupTandon <tushar.tandon118@gmail.com>
2022-02-20 03:45:35 +0530

    Version 1.0

---
+++ app/src/main/java/com/example/android/paintapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.android.paintapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.kyanogen.signatureview.SignatureView;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    SignatureView signatureView;
+    ImageButton imgEraser, imgColor, imgSave;
+    SeekBar seekBar;
+    TextView txtPenSize;
+
+    private static String fileName;
+    File path = new File(System.getenv("EXTERNAL_STORAGE") + "/myPaintings");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signatureView = findViewById(R.id.signature_view);
+        seekBar = findViewById(R.id.penSize);
+        txtPenSize = findViewById(R.id.txtPenSize);
+        imgEraser = findViewById(R.id.btnEraser);
+        imgColor = findViewById(R.id.btnColor);
+        imgSave = findViewById(R.id.btnSave);
+
+        askPermission();
+    }
+
+    private void askPermission()
+    {
+        Dexter.withContext(this)
+                .withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport)
+                    {
+                        Toast.makeText(MainActivity.this, "Granted", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken)
+                    {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
+    }
+}
